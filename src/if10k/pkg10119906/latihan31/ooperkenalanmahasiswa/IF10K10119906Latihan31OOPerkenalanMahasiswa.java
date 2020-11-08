57c9a76129445f9d20f98926c8d7f211a4ee8a79
rizqyanandar20 <rizqyananda.rusmana@gmail.com>
2020-11-08 12:51:37 +0700

    first commit

---
+++ src/if10k/pkg10119906/latihan31/ooperkenalanmahasiswa/IF10K10119906Latihan31OOPerkenalanMahasiswa.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119906.latihan31.ooperkenalanmahasiswa;
+
+/**
+ *
+ * @author senenngahenen
+ * Nama     : Rizqy Ananda Rusmana
+ * NIM      : 10119906
+ * Kelas    : IF-10K
+ */
+public class IF10K10119906Latihan31OOPerkenalanMahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        mahasiswa mhs1 = new mahasiswa();
+        mhs1.perkenalanDiri("10110269", "Rizki Adam Kurniawan");
+        
+        mahasiswa mhs2 = new mahasiswa();
+        mhs2.perkenalanDiri("10110270", "Indra Tiola");
+        
+        mahasiswa mhs3 = new mahasiswa();
+        mhs3.perkenalanDiri("10110271", "Robi Tanzil Ganefi");
+        
+        mahasiswa mhs4 = new mahasiswa();
+        mhs4.perkenalanDiri("10110269", "Muhammad Nur Awaludin");
+    }
+    
+}
---
+++ src/if10k/pkg10119906/latihan31/ooperkenalanmahasiswa/mahasiswa.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119906.latihan31.ooperkenalanmahasiswa;
+
+/**
+ *
+ * @author senenngahenen
+ * Nama     : Rizqy Ananda Rusmana
+ * NIM      : 10119906
+ * Kelas    : IF-10K
+ */
+public class mahasiswa {
+    String mahasiswa;
+    String nim;
+    
+    public void perkenalanDiri(String parNim, String parNama) {
+        System.out.println("Hallo Everyone");
+        System.out.println("My NIM is " +parNim);
+        System.out.println("My Name is " +parNama);
+        System.out.println("");
+    }
+}
